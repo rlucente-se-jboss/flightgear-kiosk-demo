743dcc357c525b64ed72228436a067b4461377c5
rlucente-se-jboss <rlucente@redhat.com>
2024-08-09 16:32:41 -0400

    add insecure registry

---
+++ config-registry.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+[[ $EUID -ne 0 ]] && exit_on_error "Must run as root"
+
+if [ "$REGISTRYINSECURE" = "true" ]
+then
+    #
+    # Setup for a local insecure registry
+    #
+    firewall-cmd --permanent --add-port=$REGISTRYPORT/tcp
+    firewall-cmd --reload
+
+    cat > /etc/containers/registries.conf.d/999-local-registry.conf <<EOF
+[[registry]]
+location = "$HOSTIP:$REGISTRYPORT"
+insecure = true
+EOF
+
+    # make local copy for later container build
+    cp /etc/containers/registries.conf.d/999-local-registry.conf .
+
+    #
+    # Create quadlet for registry service
+    #
+    mkdir -p /var/lib/registry
+
+    cat > /etc/containers/systemd/local-registry.container <<EOF
+[Unit]
+Description=A simple local registry
+
+[Container]
+Image=docker.io/library/registry:2
+ContainerName=registry
+PublishPort=$REGISTRYPORT:5000
+Volume=/var/lib/registry:/var/lib/registry:Z
+
+[Service]
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+    #
+    # Launch the local registry
+    #
+    systemctl daemon-reload
+    systemctl start local-registry
+else
+    # no-op if no insecure registry
+    echo "# nothing to see here" > /etc/containers/registries.conf.d/999-local-registry.conf
+fi
