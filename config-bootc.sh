1d80566dcfa9216965d0714853e32aa720a69395
rlucente-se-jboss <rlucente@redhat.com>
2024-08-09 16:31:17 -0400

    install needed tools for bootc

---
+++ config-bootc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+[[ $EUID -ne 0 ]] && exit_on_error "Must run as root"
+
+# some packages in EPEL require packages in the codeready-builder repo
+subscription-manager repos \
+    --enable codeready-builder-for-rhel-9-x86_64-rpms
+
+# enable the EPEL repo and install needed tools
+dnf -y install $EPEL_URL container-tools lorax
+
