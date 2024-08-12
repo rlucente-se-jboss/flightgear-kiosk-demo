6bfa8dd68a565327a3f46957743220f374afefe5
rlucente-se-jboss <rlucente@redhat.com>
2024-08-12 14:52:06 -0400

    leverage env vars

--- launch-fgfs.sh
+++ launch-fgfs.sh
@@ -5,10 +5,11 @@
 [[ $EUID -eq 0 ]] && exit_on_error "Must NOT run as root"
 
 export FG_HOME=~/.fgfs
+export FG_AIRCRAFT=$FG_HOME/Aircraft/org.flightgear.fgaddon.stable_2020/Aircraft
 
 rm -fr $FG_HOME
-mkdir -p $FG_HOME/Aircraft/org.flightgear.fgaddon.stable_2020/Aircraft
-unzip -q $(dirname $0)/F-35B.zip -d $FG_HOME/Aircraft/org.flightgear.fgaddon.stable_2020/Aircraft
+mkdir -p $FG_AIRCRAFT
+unzip -q $(dirname $0)/F-35B.zip -d $FG_AIRCRAFT
 
 
 fgfs \
@@ -23,5 +24,4 @@ fgfs \
     --enable-sentry \
     --aircraft=org.flightgear.fgaddon.stable_2020.F-35B-yasim \
     --airport=KGCN \
-    --runway=21 \
-    --fg-aircraft=$FG_HOME/Aircraft/org.flightgear.fgaddon.stable_2020/Aircraft
+    --runway=21
