10b4c1388d9016238ff50a42398e55eb8425605b
rlucente-se-jboss <rlucente@redhat.com>
2024-11-11 13:55:03 -0500

    use syspurpose correctly

--- register-and-update.sh
+++ register-and-update.sh
@@ -6,9 +6,9 @@
 
 subscription-manager register \
     --username $SCA_USER --password $SCA_PASS || exit 1
-subscription-manager role --set="RHEL Server"
-subscription-manager service-level --set="Self-Support"
-subscription-manager usage --set="Development/Test"
+subscription-manager syspurpose role --set="Red Hat Enterprise Linux Server"
+subscription-manager syspurpose service-level --set="Self-Support"
+subscription-manager syspurpose usage --set="Development/Test"
 
 dnf -y update
 dnf -y clean all
